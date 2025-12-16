51a7e0945b8371df3925985a959f4e271cbf16e2
Cakey01 <alexanderwarren7@gmail.com>
2025-12-16 12:21:26 -0500

    Create async function that gets weather data

---
+++ src/js/data.js
@@ -0,0 +1,8 @@
+export async function getData(location) {
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=days%2Ccurrent%2Calerts%2Chours&key=BQW4JJHJ6K7WPXEDJZLTMNTAB&contentType=json`);
+    return response.json();
+  } catch(error) {
+    console.log(error);
+  }
+}
