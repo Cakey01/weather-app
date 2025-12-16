63038aa25b32fce73ef3822f3000db35b93c95e2
Cakey01 <alexanderwarren7@gmail.com>
2025-12-16 12:52:05 -0500

    Add code to use input value to get weather data

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
-import '../styles.css'
-console.log('hi')
+import { getData } from './data.js';
+import { processData } from './process.js';
+
+import '../styles.css';
+
+// dom elements
+const location = document.querySelector('input');
+const submit = document.querySelector('button');
+
+submit.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const response = await getData(location.value);
+  console.log(processData(response));
+})
+
