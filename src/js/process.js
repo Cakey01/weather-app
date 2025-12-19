209bbdc7cbdb4ea47e17735fd3086c9c33e476c7
Cakey01 <alexanderwarren7@gmail.com>
2025-12-19 11:57:34 -0500

    Use split on conditions that have more than one conditions

--- src/js/process.js
+++ src/js/process.js
@@ -1,7 +1,7 @@
 export function processData(response) {
   const data = {
     current: {
-      conditions: response.currentConditions.conditions,
+      conditions: response.currentConditions.conditions.split(',')[0],
       icon: response.currentConditions.icon,
       temp: response.currentConditions.temp,
       feelsLike: response.currentConditions.feelslike,
@@ -15,20 +15,20 @@ export function processData(response) {
         time: hour.datetime,
         icon: hour.icon,
         temp: hour.temp,
-        conditions: hour.conditions,
+        conditions: hour.conditions.split(',')[0],
       })),
       tomorrow: response.days[1].hours.map((hour) => ({
         time: hour.datetime,
         icon: hour.icon,
         temp: hour.temp,
-        conditions: hour.conditions,
+        conditions: hour.conditions.split(',')[0],
       })),
     },
     week: response.days.slice(0, 8).map((day) => ({
       day: day.datetime,
       tempmax: day.tempmax,
       tempmin: day.tempmin,
-      conditions: day.conditions,
+      conditions: day.conditions.split(',')[0],
       icon: day.icon,
     })),
   };
