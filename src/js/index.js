a7296e9ab589f16581655edf220191e106a30e8c
Cakey01 <alexanderwarren7@gmail.com>
2025-12-19 13:41:57 -0500

    Set up index as controller and add event listener and handle submit

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,21 @@
 import { getData } from './data.js';
 import { processData } from './process.js';
+import { displayWeather } from './display.js';
 
 import '../styles.css';
 
-// dom elements
-const location = document.querySelector('input');
-const submit = document.querySelector('button');
+async function handleSubmit(location, unit) {
+  const response = await getData(location, unit);
+  const data = processData(response);
+  console.log(data)
+  await displayWeather(data, handleSubmit);
+}
 
-submit.addEventListener('click', async (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const response = await getData(location.value);
-  console.log(processData(response));
-})
+  const location = document.getElementById('location').value;
+  const unit = document.getElementById('units').value;
+  handleSubmit(location, unit);
+});
 
